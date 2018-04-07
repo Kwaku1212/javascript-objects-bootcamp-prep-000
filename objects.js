0750724ef13597d5350a21d65cae566d10bf0924
Kwaku J <gyazi@live.com>
2018-04-07 20:24:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,13 @@ playlist["Lil Wayne"] = "Mirror On The Wall"
 playlist["Tyler Thomas"] = "F.N.T.W"
 
 console.log(playlist)
+
+function updatePlaylist(playlist,artist,song){
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist]
+  return playlist
+}
